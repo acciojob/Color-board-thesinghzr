3c35e7b4ca4830273808d4558032c638fa3b43af
Suraj Singh <yousurajsingh@gmail.com>
2025-03-29 23:08:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+const container = document.querySelector(".container");
+const squares = 800;
+const colors = ["rgb(255, 87, 51)", "rgb(51, 255, 87)", "rgb(51, 87, 255)", "rgb(240, 51, 255)", "rgb(255, 195, 0)"];
+const defaultColor = "rgb(29, 29, 29)"; 
+
+// Create 800 squares dynamically
+for (let i = 0; i < squares; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.backgroundColor = defaultColor; 
+    square.addEventListener("mouseover", () => setColor(square));
+    square.addEventListener("mouseout", () => removeColor(square));
+
+    container.appendChild(square);
+}
+
+// Change color on hover
+function setColor(element) {
+    element.style.backgroundColor = getRandomColor();
+}
+
+// Reset color after 1 second smoothly
+function removeColor(element) {
+    setTimeout(() => {
+        element.style.backgroundColor = defaultColor;
+    }, 1000);
+}
+
+// Select a random color
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
